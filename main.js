22d57e1df26ec0f90bee87e5351a517aa0119b73
ZigmasBKatinas <ritab@RitaBs-MacBook.local>
2015-12-30 16:17:29 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+	//alert("Busy");
+
+	$('#presents').hover (
+		function(){
+			$("<h2>RANKŲ DARBO VOKAI IR ATVIRUTĖS</h2>").hide().appendTo("#presents .caption").fadeIn(200);
+		},
+		function () {
+    		$("h2:nth-child(2)").remove();
+  		}
+	);
+
+	$('#interior').hover (
+		function(){
+			$("<h2>PIEŠINIAI, PAGYVINANTYS KAMBARIO SIENAS</h2>").hide().appendTo("#interior .caption").fadeIn(200);
+		},
+		function () {
+    		$("h2:nth-child(2)").remove();
+  		}
+	);
+
+	$('#exterior').hover (
+		function(){
+			$("<h2>TEMINIAI PIEŠINIAI ANT LAUKO SIENŲ</h2>").hide().appendTo("#exterior .caption").fadeIn(200);
+		},
+		function () {
+    		$("h2:nth-child(2)").remove();
+  		}
+	);
+
+	$('#objects').hover (
+		function(){
+			$("<h2>ORIGINALIOS DAIKTŲ PUOŠYBOS IDĖJOS</h2>").hide().appendTo("#objects .caption").fadeIn(200);
+		},
+		function () {
+    		$("h2:nth-child(2)").remove();
+  		}
+	);
+
+	//collapsed navbar pushing down content
+	$('.navbar-default').on('click', function() {
+    var height = ($('.navbar-collapse.in').length == 0) ? '120px' : 0;
+    $('#services').css({marginTop: height});
+   	});
+});
+
+
+
+
+
+
