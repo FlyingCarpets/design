a6b04d88882098867885a3a807701e69ec5dfcb3
ZigmasBKatinas <ritab@RitaBs-MacBook.local>
2016-01-05 21:52:35 +0200

    js

--- main.js
+++ main.js
@@ -50,20 +50,21 @@ $(document).ready(function(){
     $('#services').css({marginTop: height});
    	});
 
-
-	//$('html,body').animate({
-  		//scrollTop: $("#scroll").offset().top
-	//});
-
-});
-
-
-$(window).load(function(){
-
 	//anchor link scroll to element
 	$('#index').localScroll({duration:800});
 	$('html,body').animate({
   		scrollTop: $('index.html#scroll').offset().top
 	});
+	
+	//$('html,body').animate({
+  		//scrollTop: $("#scroll").offset().top
+	//});
+
+
 });
+
+
+
+
+
 
